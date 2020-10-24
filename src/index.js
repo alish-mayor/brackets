2956b2f8abba909811d3eb26cd3e4c9242604882
alish-mayor <alish22061999@gmail.com>
2020-10-25 02:28:36 +0600

    feat: brackets task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    
+    let brackets = [];
+
+    bracketsConfig.forEach(element => {
+        brackets.push(element.join(""));
+    });
+    let i = 0;
+    while(i != brackets.length){
+        if (str.indexOf(brackets[i]) !== -1) {
+            str = str.replace(brackets[i], '');
+            i = 0;
+        } else {
+            i++ ;
+        }
+    }
+    return !str;
+  }
